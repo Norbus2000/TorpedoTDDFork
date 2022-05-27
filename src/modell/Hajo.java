f72a074eb19bd1c13a909c597c04276f7c165146
SZF1 A2 <szf1_a2@oktatas.local>
2022-05-27 12:45:52 +0200

    tesztSullyedt sikeres

--- src/modell/Hajo.java
+++ src/modell/Hajo.java
@@ -1,23 +1,35 @@
 package modell;
 
 public class Hajo {
+
     private int[] pozicio;
 
     public Hajo(int[] pozicio) {
         this.pozicio = pozicio;
     }
-    
-    public String talalat(int poz){
+
+    public String talalat(int poz) {
         //eldöntés tétele
         int i = 0;
-        while(i < 3 && !(pozicio[i] == poz)){
+        while (i < 3 && !(pozicio[i] == poz)) {
             i++;
         }
-        
+
         return i < 3 ? "talált" : "mellé";
     }
-    
-    public int[] getPozicio(){
+
+    public int[] getPozicio() {
         return pozicio;
+    }
+
+    public boolean isElsullyedt() {
+        boolean talalt = false;
+        for (int i = 0; i < pozicio.length; i++) {
+            if (talalat(i).equals("talált")) {
+                talalt= true;
+            } 
+        }
+        return false;
+
     }
 }
--- src/teszt/TorpedoTeszt.java
+++ src/teszt/TorpedoTeszt.java
@@ -3,24 +3,46 @@ package teszt;
 import modell.Hajo;
 
 public class TorpedoTeszt {
+    
+    private Hajo hajo;
+
+    public TorpedoTeszt() {
+         hajo = new Hajo(new int[]{2,3,4}); 
+         
+         
+         tesztLovesTalalt(4);
+         tesztLovesNemTalalt(5);
+         tesztLovesSullyedt(4);
+    }
+    
     public static void main(String[] args) {
         System.out.println("Teszt");
         
-        new TorpedoTeszt().tesztLovesTalalt(4);
-        new TorpedoTeszt().tesztLovesNemTalalt(4);
+        new TorpedoTeszt();
     }
     
     public String tesztLovesTalalt(int poz){
-        Hajo hajo = new Hajo(new int[]{2,3,4});
         String t = hajo.talalat(poz);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return "";
     }
     
-        public String tesztLovesNemTalalt(int poz){
-        Hajo hajo = new Hajo(new int[]{2,3,4});
+    public String tesztLovesNemTalalt(int poz){
         String t = hajo.talalat(poz);
         assert t.equals("mellé") : "nem jó a találat ellenőrzése";
+        return "";
+    }
+        
+    public String tesztLovesSullyedt(int poz){
+        String t = hajo.talalat(poz);
+        
+        hajo.talalat(2);
+        hajo.talalat(3);
+        hajo.talalat(4);
+        
+        boolean s = hajo.isElsullyedt();
+        
+        assert s == false  : "nem jó a süllyedt ellenőrzése";
         return "";
     }
 }
